2b051819f7114958d528c506e877dfc9e2013a4c
Rasgado Jimenez Brandon <ersonatos110@gmail.com>
2025-04-30 13:52:27 -0700

    Add files via upload

--- animals/animals/src/App.jsx
+++ animals/animals/src/App.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function getRandomAnimal() {
+  const animals = ['cat', 'bird', 'dog', 'gator', 'horse' ] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return animals(math.floor(Math.random() * animals.lenght))
 }
 
+function app(){
+  const [animals, setAnimals] = useState([])
+  const handleclick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  }
+
+
+const rederedAnimals =animals.map ((animal, index) => {
+  return <AnimalsShow type={animal} key={index}/>
+  
+}) 
+
+return(
+
+  <div className='app'>
+  <button onClick={handleclick}>add Animal</button>
+  <div className="animal-list">
+    {rendereAnimals}
+
+    </div>
+    </div>
+
+)
+}
 export default App
